62cab777b2b99d67870affc565423a3881ee7917
Buket <buketuysalkuyu@hotmail.com>
2025-06-22 22:58:02 +0100

    Add light dark mode

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,8 +3,24 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrls: ['./app.component.css']  // styles.css yerine buraya bak
 })
 export class AppComponent {
-  title = 'status-manager';
+  isDarkMode = false;
+
+  toggleDarkMode() {
+    this.isDarkMode = !this.isDarkMode;
+    const body = document.body;
+    if (this.isDarkMode) {
+      body.classList.add('dark-theme');
+      body.classList.remove('light-theme');
+    } else {
+      body.classList.add('light-theme');
+      body.classList.remove('dark-theme');
+    }
+  }
+
+  ngOnInit() {
+    document.body.classList.add('light-theme');
+  }
 }
